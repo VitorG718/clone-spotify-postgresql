12d380fad995e3db96de36b84d949147fe9cc887
VitorG718 <vitorx1280@gmail.com>
2020-11-20 19:22:17 -0300

    Modificações para melhorar a abstração do modelo e adição das tabelas de seguidores da playlist e artista

--- create-db.sql
+++ create-db.sql
@@ -1,89 +1,98 @@
-CREATE TYPE enum_genero AS ENUM('M', 'F');
+create type enum_genero as enum('M', 'F');
+create type enum_qualidade_de_download as enum ('Baixa', 'Normal', 'Alto', 'Altissima');
+create type enum_qualidade_de_reproducao as enum ('Baixa', 'Normal', 'Alto', 'Altissima');
+create type enum_idioma as enum('Português', 'English');
+create type enum_tipo as enum('desktop', 'smartphone', 'browser');
+create type enum_tipo_de_playlist as enum('U', 'R', 'S');
 
-CREATE TABLE IF NOT EXISTS usuario (
+create table if not exists usuario (
   id_usuario integer not null,
-  apelido varchar(45) not null ,
-  senha varchar(20) not null ,
-  email varchar(100) not null ,
-  data_nascimento date not null ,
+  apelido varchar(45) not null,
+  senha varchar(20) not null,
+  email varchar(100) not null,
+  data_nascimento date not null,
   genero enum_genero not null,
   primary key (id_usuario)
 );
 
-create type enum_qualidade_de_download as enum ('Baixa', 'Normal', 'Alto', 'Altissima');
-create type enum_qualidade_de_reproducao as enum ('Baixa', 'Normal', 'Alto', 'Altissima');
-create type enum_idioma as enum('Portugues', 'English');
-
-CREATE TABLE IF NOT EXISTS configuracao (
-  idioma enum_idioma NOT NULL DEFAULT 'English',
-  conteudo_explicito boolean NOT NULL DEFAULT false,
-  auto_play boolean NOT NULL DEFAULT true,
-  qualidade_de_download enum_qualidade_de_download  NOT NULL DEFAULT 'Normal',
-  qualidade_de_reproducao enum_qualidade_de_reproducao  NOT NULL DEFAULT 'Normal',
-  fk_id_usuario integer NOT null REFERENCES usuario (id_usuario),
+create table if not exists configuracao (
+  idioma enum_idioma not null default 'English',
+  conteudo_explicito boolean not null default false,
+  auto_play boolean not null default true,
+  qualidade_de_download enum_qualidade_de_download not null default 'Normal',
+  qualidade_de_reproducao enum_qualidade_de_reproducao not null default 'Normal',
+  fk_id_usuario integer not null references usuario (id_usuario),
   primary key (fk_id_usuario)
 );
 
-create type enum_tipo as ENUM('desktop', 'smartphone', 'browser');
-
-CREATE TABLE IF NOT EXISTS dispositivos_conectados (
-  id_dispositivos_conectados integer NOT NULL,
-  nome varchar(45) NOT NULL,
-  tipo enum_tipo NOT NULL,
-  fk_id_usuario integer not null REFERENCES usuario (id_usuario),
+create table if not exists dispositivos_conectados (
+  id_dispositivos_conectados integer not null,
+  nome varchar(45) not null,
+  tipo enum_tipo not null,
+  fk_id_usuario integer not null references usuario (id_usuario),
   primary key (id_dispositivos_conectados, fk_id_usuario)
 );
 
-create type enum_tipo_de_playlist as ENUM('U', 'R', 'S');
-
-CREATE TABLE IF NOT EXISTS playlist (
-  id_playlist integer NOT NULL,
-  nome varchar(45) NOT NULL,
-  duracao TIME NOT NULL,
-  tipo_de_playlist enum_tipo_de_playlist NOT NULL DEFAULT 'U',
-  privacidade boolean NOT NULL DEFAULT false,
-  total_de_musicas integer NULL DEFAULT 0,
-  seguidores integer NULL DEFAULT NULL,
-  fk_id_usuario integer  NOT null REFERENCES usuario (id_usuario),
-  PRIMARY KEY (id_playlist)
+create table if not exists playlist (
+  id_playlist integer not null,
+  nome varchar(45) not null,
+  duracao time not null default '00:00:00',
+  tipo_de_playlist enum_tipo_de_playlist not null default 'U',
+  privacidade boolean not null default false,
+  total_de_musicas integer not null default 0,
+  seguidores integer default null,
+  fk_id_usuario integer references usuario (id_usuario),
+  primary key (id_playlist)
 );
 
-CREATE TABLE IF NOT EXISTS artista (
-  id_artista integer NOT null,
-  nome varchar(45) NOT NULL,
-  biografia varchar(200) NOT NULL,
-  seguidores integer NOT NULL DEFAULT 0,
-  ouvintes_mensais integer NOT NULL DEFAULT 0,
-  verificado boolean NULL DEFAULT false,
-  PRIMARY KEY (id_artista)
+create table if not exists artista (
+  id_artista integer not null,
+  nome varchar(45) not null,
+  biografia varchar(200) not null,
+  seguidores integer not null default 0,
+  ouvintes_mensais integer not null default 0,
+  verificado boolean null default false,
+  primary key (id_artista)
 );
 
-CREATE TABLE IF NOT EXISTS album (
-  id_album integer NOT NULL,
-  nome varchar(100) NOT NULL,
-  qtd_musicas integer NOT NULL DEFAULT 0,
-  ano_lancamento integer not NULL,
-  PRIMARY KEY (id_album)
+create table if not exists seguidores_playlist (
+	fk_id_playlist integer not null references playlist (id_playlist),
+	fk_id_usuario integer not null references usuario (id_usuario),
+	primary key (fk_id_playlist, fk_id_usuario)
 );
 
-CREATE TABLE IF NOT EXISTS musica (
-  id_musica integer NOT NULL,
-  nome varchar(45) NOT NULL,
-  duracao time NOT NULL,
-  explicita boolean NOT NULL,
-  letra varchar(200) NULL DEFAULT NULL,
-  PRIMARY KEY (id_musica)
+create table if not exists seguidores_artista (
+	fk_id_artista integer not null references artista (id_artista),
+	fk_id_usuario integer not null references usuario (id_usuario),
+	primary key (fk_id_artista, fk_id_usuario)
 );
 
-CREATE TABLE IF NOT EXISTS playlist_has_musica (
-  fk_id_musica integer NOT null REFERENCES musica (id_musica),
-  fk_id_playlist integer NOT null REFERENCES playlist (id_playlist),
-  PRIMARY KEY (fk_id_playlist, fk_id_musica)
+create table if not exists album (
+  id_album integer not null,
+  nome varchar(100) not null,
+  qtd_musicas integer not null default 0,
+  ano_lancamento integer not null,
+  primary key (id_album)
 );
 
-CREATE TABLE IF NOT EXISTS artista_has_musica (
-  fk_id_artista integer NOT null REFERENCES artista (id_artista),
-  fk_id_musica integer NOT null REFERENCES musica (id_musica),
-  fk_id_album integer NOT null REFERENCES album (id_album),
-  PRIMARY KEY (fk_id_artista, fk_id_musica)
+create table if not exists musica (
+  id_musica integer not null,
+  nome varchar(45) not null,
+  duracao time not null,
+  explicita boolean not null,
+  letra varchar(200),
+  primary key (id_musica)
+);
+
+create table if not exists playlist_has_musica (
+  fk_id_musica integer not null references musica (id_musica),
+  fk_id_playlist integer not null references playlist (id_playlist),
+  primary key (fk_id_musica, fk_id_playlist)
+);
+
+create table if not exists artista_has_musica (
+  fk_id_artista integer not null references artista (id_artista),
+  fk_id_musica integer not null references musica (id_musica),
+  fk_id_album integer not null references album (id_album),
+  primary key (fk_id_artista, fk_id_musica)
 );
